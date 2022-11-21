a112ffffdf3e3e0b56974d78d4dbad5f04afdd38
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-21 10:22:44 +0300

    Backend: create the get single project view and url

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('routes/', views.getRoutes, name='routes'),
     path('projects/', views.getProjects, name='get-projects'),
+    path('projects/<str:pk>/', views.getProject, name='get-project'),
 ]
--- base/views.py
+++ base/views.py
@@ -22,3 +22,9 @@ def getProjects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def getProject(request, pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
