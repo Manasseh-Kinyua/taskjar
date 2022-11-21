06bd7483cdf721c47f73230180707a1ce430dd4e
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-21 09:22:49 +0300

    Backend: create the Task model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    handler = models.ForeignKey(User, related_name='handler', on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=250)
+    type = models.CharField(max_length=250)
+    status = models.CharField(max_length=250)
+    # message
+    description = models.TextField(null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
