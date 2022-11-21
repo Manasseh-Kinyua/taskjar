04926f60ff3f6c14c4bcf042e2cb23e0339e5c4e
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-21 09:52:15 +0300

    Backend: register models with admin site

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project, Task, Message
 
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Message)
