48e97ee9f18ce8add62c3987755f3765484375d1
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-21 11:20:03 +0300

    Frontend: create the footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
+import {  } from 'react-bootstrap'
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      my app
+      <Header />
+      <main className='py-3'>
+        <h1>Taskjar</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
