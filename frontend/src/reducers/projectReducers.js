38dd98e19e16fec249ef2a17330582da13e008de
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-21 20:17:11 +0300

    Frontend: create project list reducer

--- frontend/src/reducers/projectReducers.js
+++ frontend/src/reducers/projectReducers.js
@@ -1,30 +1,30 @@
-// import {
-//     PROJECT_LIST_REQUEST,
-//     PROJECT_LIST_SUCCESS,
-//     PROJECT_LIST_FAIL,
-// } from '../constants/projectConstants'
+import {
+    PROJECT_LIST_REQUEST,
+    PROJECT_LIST_SUCCESS,
+    PROJECT_LIST_FAIL,
+} from '../constants/projectConstants'
 
-// export const projectListReducer = (state = {projects:[]}, action) => {
-//     switch(action.type) {
-//         case PROJECT_LIST_REQUEST:
-//             return {
-//                 loading: true,
-//                 projects: []
-//             }
+export const projectListReducer = (state = {projects:[]}, action) => {
+    switch(action.type) {
+        case PROJECT_LIST_REQUEST:
+            return {
+                loading: true,
+                projects: []
+            }
 
-//         case PROJECT_LIST_SUCCESS:
-//             return {
-//                 loading: false,
-//                 projects: action.payload
-//             }
+        case PROJECT_LIST_SUCCESS:
+            return {
+                loading: false,
+                projects: action.payload
+            }
 
-//         case PROJECT_LIST_FAIL:
-//             return {
-//                 loading: false,
-//                 error: action.payload
-//             }
+        case PROJECT_LIST_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
 
-//         default:
-//             return state
-//     }
-// }
+        default:
+            return state
+    }
+}
