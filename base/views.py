53a56db8d894d9bf3202ca803e2da2ad9d214a87
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-21 09:09:10 +0300

    Backend: install DRF and run initial migrations

--- base/views.py
+++ base/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         'api/projects/',
@@ -10,4 +13,4 @@ def getRoutes(request):
         'api/projects/tasks/',
         'api/projects/tasks/:id/',
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
