cd7d572040469f11a1b7fa76bcf281150aa620b0
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-24 22:59:18 +0300

    Frontend: create the create project action

--- frontend/src/actions/projectActions.js
+++ frontend/src/actions/projectActions.js
@@ -6,8 +6,12 @@ import {
     PROJECT_DETAILS_REQUEST,
     PROJECT_DETAILS_SUCCESS,
     PROJECT_DETAILS_FAIL,
+
+    PROJECT_CREATE_REQUEST,
+    PROJECT_CREATE_SUCCESS,
+    PROJECT_CREATE_FAIL,
 } from '../constants/projectConstants'
-import { GET_ALL_PROJECTS_ENDPOINT, GET_SINGLE_PROJECT_ENDPOINT } from '../constants/apiConstants'
+import { CREATE_PROJECT_ENDPOINT, GET_ALL_PROJECTS_ENDPOINT, GET_SINGLE_PROJECT_ENDPOINT } from '../constants/apiConstants'
 import axios from 'axios'
 
 export const listProjects = () => async (dispatch) => {
@@ -45,6 +49,37 @@ export const listProjectDetails = (id) => async (dispatch) => {
     } catch(error) {
         dispatch({
             type: PROJECT_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
+
+export const createProject = (project) => async (dispatch) => {
+    try {
+        dispatch({type: PROJECT_CREATE_REQUEST})
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const {data} = await axios.get(
+            CREATE_PROJECT_ENDPOINT,
+            project,
+            config
+        )
+        
+        dispatch({
+            type: PROJECT_CREATE_SUCCESS,
+            payload: data
+        })
+
+    } catch(error) {
+        dispatch({
+            type: PROJECT_CREATE_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message
