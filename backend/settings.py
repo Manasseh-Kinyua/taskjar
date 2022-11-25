8e2e34f36728dfc067ee6538031616fdc1b79b33
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-25 10:05:51 +0300

    Backend: install simple-JWT

--- backend/settings.py
+++ backend/settings.py
@@ -56,6 +56,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
     path('routes/', views.getRoutes, name='routes'),
     path('projects/create/', views.createProject, name='create-project'),
     path('projects/', views.getProjects, name='get-projects'),
