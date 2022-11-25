29592f54bc972ef100c8025623fe0c4e3280855a
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-25 16:43:33 +0300

    Frontend: create register action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,9 +3,50 @@ import {
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGOUT,
+
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
 } from "../constants/userConstants";
-import { USER_LOGIN_ENDPOINT } from "../constants/apiConstants";
+import { USER_LOGIN_ENDPOINT, USER_REGISTER_ENDPOINT } from "../constants/apiConstants";
 import axios from 'axios'
+
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({type: USER_REGISTER_REQUEST})
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const {data} = await axios.post(
+            USER_REGISTER_ENDPOINT,
+            {'name': name, 'email': email, 'password': password},
+            config
+        )
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data
+        })
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch(error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
 
 export const login = (email, password) => async (dispatch) => {
     try {
