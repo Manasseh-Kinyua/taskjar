094db912b4ac808466cc0ceb73b669bd4ecb3d4c
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-29 12:06:47 +0300

    Frontend: create task list action

---
+++ frontend/src/actions/taskActions.js
@@ -0,0 +1,42 @@
+import {
+    TASK_LIST_REQUEST,
+    TASK_LIST_SUCCESS,
+    TASK_LIST_FAIL,
+} from "../constants/taskConstants";
+import { GET_PROJECT_TASKS_ENDPOINT } from "../constants/apiConstants";
+import axios from 'axios'
+
+export const listTasks = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({type: TASK_LIST_REQUEST})
+
+        const {
+            userLogin: {userInfo}
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authirization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.get(
+            `${GET_PROJECT_TASKS_ENDPOINT}${id}/`,
+            config
+        )
+
+        dispatch({
+            type: TASK_LIST_SUCCESS,
+            payload: data
+        })
+
+    } catch(error) {
+        dispatch({
+            type: TASK_LIST_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
