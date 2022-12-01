b6ec89a3581fff2520a8fb66a722bbb60d26093a
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-01 16:47:58 +0300

    Backend: fix bug in create task url

--- base/urls/task_urls.py
+++ base/urls/task_urls.py
@@ -3,10 +3,10 @@ from base.views import task_views as views
 
 urlpatterns = [
     path('for-project/<str:pk>/', views.getProjectTasks, name='project-tasks'),
+    path('create/', views.createTask, name='create-task'),
     path('<str:pk>/', views.getSingleTask, name='single-task'),
     path('edit/<str:pk>/', views.editTask, name='edit-task'),
-    path('update/to-in-progress/<str:pk>/', views.editTask, name='edit-task'),
-    path('update/to-done/<str:pk>/', views.editTask, name='edit-task'),
+    path('update/to-in-progress/<str:pk>/', views.updateTaskToInProgress, name='in-progress-task'),
+    path('update/to-done/<str:pk>/', views.updateTaskToDone, name='to-done-task'),
     path('delete/<str:pk>/', views.deleteTask, name='delete-task'),
-    path('create/', views.createTask, name='create-task'),
 ]
