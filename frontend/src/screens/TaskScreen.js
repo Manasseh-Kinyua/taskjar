0d11c8b1141c1373b8c4b8dba2e1b73b74a902fb
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-02 14:32:55 +0300

    Frontend: dispatch message create action

--- frontend/src/screens/TaskScreen.js
+++ frontend/src/screens/TaskScreen.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams, useLocation, Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { listTaskDetails } from '../actions/taskActions'
+import { createTaskMessage, listTaskDetails } from '../actions/taskActions'
 import Container from '@mui/material/Container';
 import Chip from '@mui/material/Chip';
 import Avatar from '@mui/material/Avatar';
-import { Row, Col, ListGroup, Card } from 'react-bootstrap'
+import { Row, Col, ListGroup, Form } from 'react-bootstrap'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
 function TaskScreen() {
+
+    const [body, setBody] = useState('')
 
     const params = useParams()
 
@@ -30,13 +32,29 @@ function TaskScreen() {
     const {loading, error, task} = taskDetail
     console.log(task)
 
+    const taskCreateMessage = useSelector(state => state.taskCreateMessage)
+    const {loading: loadingCreateMessage, error: errorCreateMessage, success: successCreateMessage, message} = taskCreateMessage
+    console.log(message)
+
     useEffect(() => {
       if(!userInfo) {
         navigate('/login')
       } else {
         dispatch(listTaskDetails(params.id))
       }
-    }, [dispatch, userInfo, navigate, params.id])
+      if(successCreateMessage) {
+        setBody('')
+      }
+    }, [dispatch, userInfo, navigate, params.id, successCreateMessage])
+
+    const submitMessageHandler = (e) => {
+      e.preventDefault()
+
+      dispatch(createTaskMessage({
+        task_id: params.id,
+        body
+      }))
+    }
 
   return (
     <div>
@@ -80,19 +98,29 @@ function TaskScreen() {
               <ListGroup.Item style={{width:'95%', border:'.1rem solid grey', margin:'1rem auto'}}>
                 <h6>Conversation</h6>
                 {task.messages && task.messages.map(message => (
-                  <div className='p-1' style={{borderLeft:'.2rem solid rgb(208, 41, 208)'}} key={message.id}>
+                  <div className='m-1 p-1' style={{borderLeft:'.2rem solid rgb(208, 41, 208)'}} key={message.id}>
                     <span style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
                       <span style={{display:'flex', alignItems:'center'}}>
                         <Avatar sx={{ width: 40, height: 40 }} alt="Remy Sharp" src="https://www.shutterstock.com/image-illustration/male-profile-picture-silhouette-avatar-260nw-149293406.jpg" />
-                        <small> @{message.user.name}</small>
+                        <small style={{marginLeft:'.5rem', color:'#20c997'}}> @{message.user.name}</small>
                       </span>
                       <small>{message.createdAt && message.createdAt.substring(0,10)}</small>
                     </span>
-                    {/* <span style={{marginLeft:'1rem'}}> */}
-                      <p style={{marginLeft:'2.7rem'}}>{message.body}</p>
-                    {/* </span> */}
+                      <p style={{marginLeft:'3rem'}}>{message.body}</p>
                 </div>
                 ))}
+                <Form className='p-1' onSubmit={submitMessageHandler}>
+                  {loadingCreateMessage && <Loader/>}
+                  {errorCreateMessage && <Message variant='warning'>{errorCreateMessage}</Message>}
+                  <Form.Group>
+                    <Form.Control
+                      type='text'
+                      placeholder='Write your message here'
+                      value={body}
+                      onChange={(e) => setBody(e.target.value)}
+                      ></Form.Control>
+                  </Form.Group>
+                </Form>
               </ListGroup.Item>
             </ListGroup>
           </Col>
