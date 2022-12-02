23fd7be7463686e2d65c4cd3aa441b67e67c31aa
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-02 15:49:10 +0300

    Frontend: create task edit actioin

--- frontend/src/actions/taskActions.js
+++ frontend/src/actions/taskActions.js
@@ -11,6 +11,10 @@ import {
     CREATE_TASK_SUCCESS,
     CREATE_TASK_FAIL,
 
+    EDIT_TASK_REQUEST,
+    EDIT_TASK_SUCCESS,
+    EDIT_TASK_FAIL,
+
     CREATE_TASK_MESSAGE_REQUEST,
     CREATE_TASK_MESSAGE_SUCCESS,
     CREATE_TASK_MESSAGE_FAIL,
@@ -19,7 +23,7 @@ import {
     DELETE_TASK_SUCCESS,
     DELETE_TASK_FAIL,
 } from "../constants/taskConstants";
-import { CREATE_TASK_ENDPOINT, CREATE_TASK_MESSAGE_ENDPOINT, DELETE_TASK_ENDPOINT, GET_PROJECT_TASKS_ENDPOINT, GET_SINGLE_TASK_ENDPOINT } from "../constants/apiConstants";
+import { CREATE_TASK_ENDPOINT, CREATE_TASK_MESSAGE_ENDPOINT, DELETE_TASK_ENDPOINT, EDIT_TASK_ENDPOINT, GET_PROJECT_TASKS_ENDPOINT, GET_SINGLE_TASK_ENDPOINT } from "../constants/apiConstants";
 import axios from 'axios'
 
 export const listTasks = (id) => async (dispatch, getState) => {
@@ -121,6 +125,47 @@ export const createTask = (task) => async (dispatch, getState) => {
     } catch(error) {
         dispatch({
             type: CREATE_TASK_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
+
+export const editTask = (task) => async (dispatch, getState) => {
+    try {
+        dispatch({type: EDIT_TASK_REQUEST})
+
+        const {
+            userLogin: {userInfo}
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.put(
+            `${EDIT_TASK_ENDPOINT}${task.id}`,
+            task,
+            config
+        )
+
+        dispatch({
+            type: EDIT_TASK_SUCCESS,
+            payload: data
+        })
+
+        dispatch({
+            type: TASK_DETAIL_SUCCESS,
+            payload: data
+        })
+
+    } catch(error) {
+        dispatch({
+            type: EDIT_TASK_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message
