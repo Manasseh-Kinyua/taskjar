d901262a95c313e070466000dd36321102a3d6c9
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-05 15:17:41 +0300

    Frontend: dispatch user delete action

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -6,68 +6,83 @@ import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Table, Button } from 'react-bootstrap'
-import { getAllUsers } from '../actions/userActions';
-import { Link } from 'react-router-dom'
+import { deleteUser, getAllUsers } from '../actions/userActions';
+import { Link, useParams } from 'react-router-dom'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function UserListScreen() {
+
+    const params = useParams()
 
     const dispatch = useDispatch()
 
     const allUsers = useSelector(state => state.allUsers)
     const {loading, error, users} = allUsers
 
+    const userDelete = useSelector(state => state.userDelete)
+    const {loading: loadingDeleteUser, error: errorDeleteUser, success: successDeleteUser} = userDelete
+
     useEffect(() => {
         dispatch(getAllUsers())
-    }, [dispatch])
+    }, [dispatch, successDeleteUser])
 
     const deleteUserHandler = (id) => {
-
+        if(window.confirm("Are you sure you want to delete this user")) {
+            dispatch(deleteUser(id))
+        }
     }
 
   return (
     <div>
       <Container maxWidth='xl'>
         <h1>users</h1>
-        <Table responsive striped  hover  >
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>NAME</th>
-                    <th>EMAIL</th>
-                    <th>ADMIN</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {users && users.map(user => (
-                    <tr key={user.id}>
-                        <td>{user.id}</td>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>
-                            {user.isAdmin ? (
-                                <CheckCircleOutlineIcon style={{color:'green'}}/>
-                            ) : (
-                                <HighlightOffIcon style={{color:'red'}}/>
-                            )}
-                        </td>
-                        <td>
-                            <Link to={`/user/edit/${user.id}`}>
-                                <Button  className='btn-sm bg-dark'>
-                                    <EditIcon style={{color:'green'}} />
-                                </Button>
-                            </Link>
-                        </td>
-                        <td>
-                            <Button onClick={() => deleteUserHandler(user.id)}  className='btn-sm bg-dark'>
-                                <DeleteIcon style={{color:'red'}} />
-                             </Button>
-                        </td>
+        {loading ? (
+            <Loader />
+        ) : error ? (
+            <Message varinat="warning">{error}</Message>
+        ) : (
+            <Table responsive striped  hover  >
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>NAME</th>
+                        <th>EMAIL</th>
+                        <th>ADMIN</th>
+                        <th></th>
+                        <th></th>
                     </tr>
-                ))}
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                    {users && users.map(user => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>
+                                {user.isAdmin ? (
+                                    <CheckCircleOutlineIcon style={{color:'green'}}/>
+                                ) : (
+                                    <HighlightOffIcon style={{color:'red'}}/>
+                                )}
+                            </td>
+                            <td>
+                                <Link to={`/user/edit/${user.id}`}>
+                                    <Button  className='btn-sm bg-dark'>
+                                        <EditIcon style={{color:'green'}} />
+                                    </Button>
+                                </Link>
+                            </td>
+                            <td>
+                                <Button onClick={() => deleteUserHandler(user.id)}  className='btn-sm bg-dark'>
+                                    <DeleteIcon style={{color:'red'}} />
+                                </Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        )}
       </Container>
     </div>
   )
