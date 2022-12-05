df566c8b50aa32abf8d4bddad48b820383e6c199
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-05 12:40:28 +0300

    Backend: all users view and url

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.registerUser, name='register'),
     path('profile/', views.getUserProfile, name='user-profile'),
+    path('', views.getAllUsers, name='users'),
 ]
--- base/views/user_views.py
+++ base/views/user_views.py
@@ -50,3 +50,10 @@ def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getAllUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
