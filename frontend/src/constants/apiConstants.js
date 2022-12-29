76b71e6e7326e75e63d7a12a1f55b8fb4ad1af9b
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-29 14:39:58 +0300

    use https

--- frontend/src/constants/apiConstants.js
+++ frontend/src/constants/apiConstants.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://127.0.0.1:8000/api/"
-export const BASE_URL = "http://taskjar.up.railway.app/api/"
+export const BASE_URL = "https://taskjar.up.railway.app/api/"
 
 export const GET_ALL_PROJECTS_ENDPOINT = `${BASE_URL}projects/`
 export const GET_SINGLE_PROJECT_ENDPOINT = `${BASE_URL}projects/`
